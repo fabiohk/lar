5b6ea2f8b3c73958c09d99d7080d8b946c051813
Fabio Kubo <fabio_harada@hotmail.com>
2022-04-12 23:19:37 -0300

    feat(hackerrank): add solution for ice cream parlor

---
+++ hackerrank/interview-preparation-kit/search/ice_cream_parlor.py
@@ -0,0 +1,35 @@
+import enum
+from collections import defaultdict
+from typing import Dict, List, NamedTuple, Sequence
+
+
+class Solution(NamedTuple):
+    smaller_id: int
+    larger_id: int
+
+
+def solver(costs: Sequence[int], money: int) -> Solution:
+    costs_and_ids_map = build_costs_and_ids_map(costs)
+
+    for cost, ids in costs_and_ids_map.items():
+        money_left = money - cost
+        if money_left in costs_and_ids_map:
+            if money_left != cost or (money_left == cost and has_more_than_one_id(costs_and_ids_map[money_left])):
+                smaller_id = ids[0]
+                larger_id_idx = 0 if money_left != cost else 1
+                larger_id = costs_and_ids_map[money_left][larger_id_idx]
+                print(f"{smaller_id} {larger_id}")
+                return Solution(smaller_id, larger_id)
+
+    raise Exception("No Solution could be found!")
+
+
+def build_costs_and_ids_map(costs: Sequence[int]) -> Dict[int, List[int]]:
+    costs_and_ids_map = defaultdict(lambda: [])
+    for id, cost in enumerate(costs, start=1):
+        costs_and_ids_map[cost].append(id)
+    return costs_and_ids_map
+
+
+def has_more_than_one_id(ids: Sequence[int]) -> bool:
+    return len(ids) > 1
---
+++ hackerrank/interview-preparation-kit/search/test_ice_cream_parlor.py
@@ -0,0 +1,16 @@
+from typing import Sequence
+
+import pytest
+
+from ice_cream_parlor import Solution, solver
+
+
+@pytest.mark.parametrize(
+    "cost, money, expected_smaller_id, expected_larger_id", (([1, 4, 5, 3, 2], 4, 1, 4), ([2, 2, 4, 3], 4, 1, 2))
+)
+def test_should_gives_expected_output(
+    cost: Sequence[int], money: int, expected_smaller_id: int, expected_larger_id: int
+):
+    solution: Solution = solver(cost, money)
+    assert solution.smaller_id == expected_smaller_id
+    assert solution.larger_id == expected_larger_id
