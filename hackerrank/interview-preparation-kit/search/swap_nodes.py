6bf8b5b5f3ee0d2e598d8ecefea1508f96948701
Fabio Kubo <fabio_harada@hotmail.com>
2022-09-28 23:47:13 -0300

    feat(hackerrank): add in order traversal for wip solution for swap nodes

--- hackerrank/interview-preparation-kit/search/swap_nodes.py
+++ hackerrank/interview-preparation-kit/search/swap_nodes.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from tkinter.messagebox import NO
 from typing import List, Optional, Sequence
+from xmlrpc.client import boolean
 
 
 @dataclass
@@ -7,12 +9,11 @@ class Node:
     value: int
     left: Optional["Node"] = None
     right: Optional["Node"] = None
+    visited: boolean = False
+    in_queue_to_visit: boolean = False
 
 
-def solution(
-    indexes: Sequence[Sequence[int]], queries: Sequence[int]
-) -> List[List[int]]:
-    # build binary tree
+def build_binary_tree(indexes: Sequence[Sequence[int]]) -> List[Node]:
     root = Node(1)
     nodes = [root]
     evaluating_node = root
@@ -33,7 +34,52 @@ def solution(
         if i + 1 < len(nodes):
             evaluating_node = nodes[i + 1]
 
+    return nodes
+
+
+def should_visit_node_in_future(node: Optional[Node]) -> bool:
+    return node is not None and not node.visited and not node.in_queue_to_visit
+
+
+def run_in_order_traversal(binary_tree: Sequence[Node]) -> List[int]:
+    root = binary_tree[0]
+    to_visit_nodes = [root]
+    visited_nodes = []
+
+    while len(to_visit_nodes) > 0:
+        current_visited_node: Node = to_visit_nodes.pop()
+        current_visited_node.in_queue_to_visit = False
+        if should_visit_node_in_future(current_visited_node.right):
+            to_visit_nodes.append(current_visited_node.right)
+            current_visited_node.right.in_queue_to_visit = True
+
+        if should_visit_node_in_future(current_visited_node.left):
+            to_visit_nodes.append(current_visited_node)
+            to_visit_nodes.append(current_visited_node.left)
+            current_visited_node.left.in_queue_to_visit = True
+            current_visited_node.in_queue_to_visit = True
+        elif not current_visited_node.visited:
+            visited_nodes.append(current_visited_node.value)
+            current_visited_node.visited = True
+
+    return visited_nodes
+
+
+def swap_nodes(binary_tree: Sequence[Node], k: int) -> List[Node]:
+    return binary_tree
+
+
+def solution(
+    indexes: Sequence[Sequence[int]], queries: Sequence[int]
+) -> List[List[int]]:
+    # build binary tree
+    binary_tree = build_binary_tree(indexes)
+
+    results = []
     # for each query, swap childs
-    # run in-order and append result
-    print(nodes)
-    return []
+    for k in queries:
+        binary_tree = swap_nodes(binary_tree, k)
+        in_order_traversal = run_in_order_traversal(binary_tree)
+        results.append(in_order_traversal)
+
+    return results
