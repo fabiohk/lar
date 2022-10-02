30ba4f5d94c777764a68aaac6bb5823f244d2ede
Fabio Kubo <fabio_harada@hotmail.com>
2022-10-02 00:36:00 -0300

    feat(hackerrank): add swap node algorithm for search/swap_nodes

--- hackerrank/interview-preparation-kit/search/swap_nodes.py
+++ hackerrank/interview-preparation-kit/search/swap_nodes.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from platform import node
+from re import L
 from turtle import left
 from typing import Generator, List, Sequence
 
@@ -7,8 +8,14 @@ from typing import Generator, List, Sequence
 @dataclass
 class Node:
     value: int
-    in_queue_to_visit: bool = False
-    visited_node: bool = False
+
+
+def possible_left_idx(node_idx: int) -> int:
+    return (node_idx + 1) * 2 - 1
+
+
+def possible_right_idx(node_idx: int) -> int:
+    return (node_idx + 1) * 2
 
 
 def next_valid_node(nodes: List[Node], possible_valid_node_idx: int) -> int:
@@ -28,47 +35,91 @@ def build_binary_tree(indexes: Sequence[Sequence[int]]) -> List[Node]:
     return nodes
 
 
-def should_visit_node(node: Node) -> bool:
-    return node.value != -1 and not node.in_queue_to_visit and not node.visited_node
+def should_visit_node(node: Node, in_queue_to_visit: bool, visited_node: bool) -> bool:
+    return node.value != -1 and not in_queue_to_visit and not visited_node
 
 
 def inorder_nodes(binary_tree: Sequence[Node]) -> Generator[Node, None, None]:
     to_visit_nodes_indices = [0]
+    in_queue_map = [False for _ in range(len(binary_tree))]
+    visited_node_map = [False for _ in range(len(binary_tree))]
 
     while len(to_visit_nodes_indices) > 0:
         node_idx = to_visit_nodes_indices.pop()
         node = binary_tree[node_idx]
-        node.in_queue_to_visit = False
-        possible_left_child_idx = (node_idx + 1) * 2 - 1
-        possible_right_child_idx = (node_idx + 1) * 2
+        in_queue_map[node_idx] = False
+        possible_left_child_idx = possible_left_idx(node_idx)
+        possible_right_child_idx = possible_right_idx(node_idx)
 
         if possible_left_child_idx >= len(binary_tree):
             yield node
-            node.visited_node = True
+            visited_node_map[node_idx] = True
             continue
 
         left_child = binary_tree[possible_left_child_idx]
         right_child = binary_tree[possible_right_child_idx]
 
-        if should_visit_node(right_child):
+        if should_visit_node(
+            right_child,
+            in_queue_map[possible_right_child_idx],
+            visited_node_map[possible_right_child_idx],
+        ):
             to_visit_nodes_indices.append(possible_right_child_idx)
-            right_child.in_queue_to_visit = True
+            in_queue_map[possible_right_child_idx] = True
 
-        if should_visit_node(left_child):
+        if should_visit_node(
+            left_child,
+            in_queue_map[possible_left_child_idx],
+            visited_node_map[possible_left_child_idx],
+        ):
             to_visit_nodes_indices.append(node_idx)
-            node.in_queue_to_visit = True
+            in_queue_map[node_idx] = True
             to_visit_nodes_indices.append(possible_left_child_idx)
-            left_child.in_queue_to_visit = True
+            in_queue_map[possible_left_child_idx] = True
         else:
             yield node
-            node.visited_node = True
+            visited_node_map[node_idx] = True
 
 
 def run_in_order_traversal(binary_tree: Sequence[Node]) -> List[int]:
     return [node.value for node in inorder_nodes(binary_tree) if node.value != -1]
 
 
+def nodes_idx_from_depth(depth: int) -> Generator[int, None, None]:
+    start = 2 ** (depth - 1) - 1
+    not_inclusive_end = 2 ** (depth) - 1
+    for i in range(start, not_inclusive_end):
+        yield i
+
+
+def swap_elements_from_list(
+    list_to_swap: Sequence[Node], idx_a: int, idx_b: int
+) -> Sequence[Node]:
+    list_to_swap[idx_a], list_to_swap[idx_b] = list_to_swap[idx_b], list_to_swap[idx_a]
+    return list_to_swap
+
+
+def swap_childs_from_node(binary_tree: Sequence[Node], node_idx: int) -> List[Node]:
+    possible_left_child_idx = possible_left_idx(node_idx)
+    possible_right_child_idx = possible_right_idx(node_idx)
+
+    if possible_left_child_idx >= len(binary_tree):
+        return binary_tree
+
+    return swap_elements_from_list(
+        binary_tree, possible_left_child_idx, possible_right_child_idx
+    )
+
+
 def swap_nodes(binary_tree: Sequence[Node], k: int) -> List[Node]:
+    tree_depth = len(binary_tree) // 2 + 1
+    h = k
+
+    while h < tree_depth:
+        for node_idx in nodes_idx_from_depth(h):
+            binary_tree = swap_childs_from_node(binary_tree, node_idx)
+        h += k
+
     return binary_tree
 
 
