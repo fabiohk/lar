7de4ff93caf6f2f8a506bfeaed7cab36b91665df
Fabio Kubo <fabio_harada@hotmail.com>
2022-10-02 00:00:01 -0300

    refactor(hackerrank): simplify data structure for search problem

--- hackerrank/interview-preparation-kit/search/swap_nodes.py
+++ hackerrank/interview-preparation-kit/search/swap_nodes.py
@@ -1,68 +1,71 @@
 from dataclasses import dataclass
-from tkinter.messagebox import NO
-from typing import List, Optional, Sequence
-from xmlrpc.client import boolean
+from platform import node
+from turtle import left
+from typing import Generator, List, Sequence
 
 
 @dataclass
 class Node:
     value: int
-    left: Optional["Node"] = None
-    right: Optional["Node"] = None
-    visited: boolean = False
-    in_queue_to_visit: boolean = False
+    in_queue_to_visit: bool = False
+    visited_node: bool = False
+
+
+def next_valid_node(nodes: List[Node], possible_valid_node_idx: int) -> int:
+    while nodes[possible_valid_node_idx].value == -1:
+        possible_valid_node_idx += 1
+    return possible_valid_node_idx
 
 
 def build_binary_tree(indexes: Sequence[Sequence[int]]) -> List[Node]:
-    root = Node(1)
-    nodes = [root]
-    evaluating_node = root
-
-    for i, idx in enumerate(indexes):
-        left, right = idx
-
-        if left != -1:
-            left_node = Node(left)
-            nodes.append(left_node)
-            evaluating_node.left = left_node
-
-        if right != -1:
-            right_node = Node(right)
-            nodes.append(right_node)
-            evaluating_node.right = right_node
-
-        if i + 1 < len(nodes):
-            evaluating_node = nodes[i + 1]
+    nodes = [Node(1)]
+    node_idx = -1
+    for left, right in indexes:
+        node_idx = next_valid_node(nodes, node_idx)
+        nodes.append(Node(left))
+        nodes.append(Node(right))
 
     return nodes
 
 
-def should_visit_node_in_future(node: Optional[Node]) -> bool:
-    return node is not None and not node.visited and not node.in_queue_to_visit
+def should_visit_node(node: Node) -> bool:
+    return node.value != -1 and not node.in_queue_to_visit and not node.visited_node
+
+
+def inorder_nodes(binary_tree: Sequence[Node]) -> Generator[Node, None, None]:
+    to_visit_nodes_indices = [0]
+
+    while len(to_visit_nodes_indices) > 0:
+        node_idx = to_visit_nodes_indices.pop()
+        node = binary_tree[node_idx]
+        node.in_queue_to_visit = False
+        possible_left_child_idx = (node_idx + 1) * 2 - 1
+        possible_right_child_idx = (node_idx + 1) * 2
+
+        if possible_left_child_idx >= len(binary_tree):
+            yield node
+            node.visited_node = True
+            continue
+
+        left_child = binary_tree[possible_left_child_idx]
+        right_child = binary_tree[possible_right_child_idx]
+
+        if should_visit_node(right_child):
+            to_visit_nodes_indices.append(possible_right_child_idx)
+            right_child.in_queue_to_visit = True
+
+        if should_visit_node(left_child):
+            to_visit_nodes_indices.append(node_idx)
+            node.in_queue_to_visit = True
+            to_visit_nodes_indices.append(possible_left_child_idx)
+            left_child.in_queue_to_visit = True
+        else:
+            yield node
+            node.visited_node = True
 
 
 def run_in_order_traversal(binary_tree: Sequence[Node]) -> List[int]:
-    root = binary_tree[0]
-    to_visit_nodes = [root]
-    visited_nodes = []
-
-    while len(to_visit_nodes) > 0:
-        current_visited_node: Node = to_visit_nodes.pop()
-        current_visited_node.in_queue_to_visit = False
-        if should_visit_node_in_future(current_visited_node.right):
-            to_visit_nodes.append(current_visited_node.right)
-            current_visited_node.right.in_queue_to_visit = True
-
-        if should_visit_node_in_future(current_visited_node.left):
-            to_visit_nodes.append(current_visited_node)
-            to_visit_nodes.append(current_visited_node.left)
-            current_visited_node.left.in_queue_to_visit = True
-            current_visited_node.in_queue_to_visit = True
-        elif not current_visited_node.visited:
-            visited_nodes.append(current_visited_node.value)
-            current_visited_node.visited = True
-
-    return visited_nodes
+    return [node.value for node in inorder_nodes(binary_tree) if node.value != -1]
 
 
 def swap_nodes(binary_tree: Sequence[Node], k: int) -> List[Node]:
