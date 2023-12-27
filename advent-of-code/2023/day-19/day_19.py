362119bc670924c830fc42a864bab4a4d878bf7e
Fabio Kubo <fabio_harada@hotmail.com>
2023-12-26 23:56:33 -0300

    feat(advent-of-code): add 19th day second part solution

--- advent-of-code/2023/day-19/day_19.py
+++ advent-of-code/2023/day-19/day_19.py
@@ -1,8 +1,13 @@
 from collections.abc import Callable
 from dataclasses import dataclass
 from enum import StrEnum
+from queue import SimpleQueue
 import re
+from typing import Optional
 
+class Operator(StrEnum):
+    LESS_THAN = "<"
+    GREATER_THAN = ">"
 
 class Result(StrEnum):
     ACCEPTED = "A"
@@ -13,10 +18,29 @@ type Rating = dict[str, int]
 type Rule = Callable[[Rating], str] # returns an empty string when the rule is not satisfied, otherwise returns the next workflow
 
 @dataclass
+class Operation:
+    rating_key: str
+    operator: Operator
+    value: int
+
+@dataclass
+class RuleV2:
+    next_workflow: str
+    operation: Optional[Operation] = None
+
+@dataclass
 class Workflow:
     rules: list[Rule]
+    rules_v2: list[RuleV2]
 
 type Workflows = dict[str, Workflow]
+
+@dataclass
+class Interval:
+    min: int
+    max: int
+
+type Intervals = dict[str, Interval]
 
 def run_rating_through_workflow(rating: Rating, workflows: Workflows) -> Result:
     current_workflow = "in"
@@ -58,7 +82,27 @@ def parse_workflow(line: str) -> tuple[str, Workflow]:
     return label, parse_rules(rules)
 
 def parse_rules(rules: list[str]) -> Workflow:
-    return Workflow([parse_rule(rule) for rule in rules])
+    return Workflow([parse_rule(rule) for rule in rules], [parse_rule_v2(rule) for rule in rules])
+
+def parse_rule_v2(rule: str) -> RuleV2:
+    if ":" in rule:
+        return parse_rule_with_operator_v2(rule)
+    return RuleV2(rule)
+
+def parse_rule_with_operator_v2(rule: str) -> RuleV2:
+    if "<" in rule:
+        return parse_lt_condition_v2(rule)
+    return parse_gt_condition_v2(rule)
+
+def parse_lt_condition_v2(rule: str) -> RuleV2:
+    condition, next_workflow = rule.split(":")
+    key, value = condition.split("<")
+    return RuleV2(next_workflow, Operation(key, Operator.LESS_THAN, int(value)))
+
+def parse_gt_condition_v2(rule: str) -> RuleV2:
+    condition, next_workflow = rule.split(":")
+    key, value = condition.split(">")
+    return RuleV2(next_workflow, Operation(key, Operator.GREATER_THAN, int(value)))
 
 def parse_rule(rule: str) -> Rule:
     if ":" in rule:
@@ -67,10 +111,10 @@ def parse_rule(rule: str) -> Rule:
 
 def parse_rule_with_operator(rule: str) -> Rule:
     if "<" in rule:
-        return parse_le_condition(rule)
+        return parse_lt_condition(rule)
     return parse_gt_condition(rule)
 
-def parse_le_condition(rule: str) -> Rule:
+def parse_lt_condition(rule: str) -> Rule:
     condition, next_workflow = rule.split(":")
     key, value = condition.split("<")
     return lambda rating: next_workflow if rating[key] < int(value) else ""
@@ -103,8 +147,81 @@ def sum_all_ratings_from_accepted_parts(workflows: Workflows, ratings: list[Rati
     return total_sum
 
 
+def find_accepted_intervals(workflows: Workflows) -> list[Intervals]:
+    starting_intervals = {
+        "x": Interval(0, 4001),
+        "m": Interval(0, 4001),
+        "a": Interval(0, 4001),
+        "s": Interval(0, 4001)
+    }
+    accepted_intervals: list[Intervals] = []
+    queue = SimpleQueue()
+    queue.put(("in", starting_intervals))
+
+    while not queue.empty():
+        workflow, intervals = queue.get()
+        if workflow == Result.ACCEPTED:
+            accepted_intervals.append(intervals)
+            continue
+        if workflow == Result.REJECTED:
+            continue
+        for rule in workflows[workflow].rules_v2:
+            new_intervals, intervals = adjust_intervals(intervals, rule)
+            next_workflow = rule.next_workflow
+            queue.put((next_workflow, new_intervals))
+    return accepted_intervals
+
+def adjust_intervals(intervals: Intervals, rule: RuleV2) -> tuple[Intervals, Intervals]:
+    if not rule.operation:
+        return intervals, intervals
+    
+    if rule.operation.operator == Operator.LESS_THAN:
+        return adjust_intervals_with_lt_operator(intervals, rule.operation)
+    return adjust_intervals_with_gt_operator(intervals, rule.operation)
+
+def adjust_intervals_with_lt_operator(intervals: Intervals, operation: Operation) -> tuple[Intervals, Intervals]:
+    new_intervals = {}
+    opposite_intervals = {}
+
+    for key, interval in intervals.items():
+        if key != operation.rating_key:
+            new_intervals[key] = interval
+            opposite_intervals[key] = interval
+        else:
+            new_intervals[key] = Interval(interval.min, operation.value)
+            opposite_intervals[key] = Interval(operation.value - 1, interval.max)
+    
+    return new_intervals, opposite_intervals
+
+def adjust_intervals_with_gt_operator(intervals: Intervals, operation: Operation) -> tuple[Intervals, Intervals]:
+    new_intervals = {}
+    opposite_intervals = {}
+
+    for key, interval in intervals.items():
+        if key != operation.rating_key:
+            new_intervals[key] = interval
+            opposite_intervals[key] = interval
+        else:
+            new_intervals[key] = Interval(operation.value, interval.max)
+            opposite_intervals[key] = Interval(interval.min, operation.value + 1)
+    
+    return new_intervals, opposite_intervals
+
+
+def count_possible_combinations(workflows: Workflows) -> int:
+    accepted_intervals = find_accepted_intervals(workflows)
+    return sum(count_combination_from_intervals(intervals) for intervals in accepted_intervals)
+
+def count_combination_from_intervals(intervals: Intervals) -> int:
+    total_combinations = 1
+    for interval in intervals.values():
+        total_combinations *= (interval.max - interval.min - 1)
+    return total_combinations
+
 workflows, ratings = read_input("./example")
 print(sum_all_ratings_from_accepted_parts(workflows, ratings))
+print(count_possible_combinations(workflows))
 
 workflows, ratings = read_input("./input")
 print(sum_all_ratings_from_accepted_parts(workflows, ratings))
+print(count_possible_combinations(workflows))
